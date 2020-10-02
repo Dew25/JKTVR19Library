3f5f6754ee37989b52e519b43377037c07a37e56
Juri Melnikov <Juri.Melnikov@ivkhk.ee>
2020-10-02 14:14:25 +0300

    Заготовка проекта

---
+++ src/jktvr19library/App.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jktvr19library;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Melnikov
+ */
+public class App {
+    private Scanner scanner = new Scanner(System.in);
+
+    public void run() {
+        System.out.println("--- Библиотека ---");
+        boolean repeat = true;
+        do{
+            System.out.println("=============================");
+            System.out.println("Задачи:");
+            System.out.println("0. Выйти из программы");
+            System.out.println("1. Добавить книгу");
+            System.out.println("2. Список книг");
+            System.out.println("3. Добавить читателя");
+            System.out.println("4. Список читателей");
+            System.out.println("5. Выдать книгу");
+            System.out.println("6. Вернуть книгу");
+            System.out.print("Выберите задачу: ");
+            String task = scanner.nextLine();
+            System.out.println("=============================");
+            switch (task) {
+                case "0":
+                    System.out.println("--- конец программы ---");
+                    repeat = false;
+                    break;
+                case "1":
+                    System.out.println("--- Добавить книгу ---");
+                    break;
+                case "2":
+                    System.out.println("--- Список книг ---");
+                    break;
+                case "3":
+                    System.out.println("--- Добавить читателя ---");
+                    break;
+                case "4":
+                    System.out.println("--- Список читателей ---");
+                    break;
+                case "5":
+                    System.out.println("--- Выдать книгу ---");
+                    break;
+                case "6":
+                    System.out.println("--- Вернуть книгу ---");
+                    break;
+                default:
+                    System.out.println("Нет такой задачи");;
+            }
+        }while(repeat);
+    }
+
+}
